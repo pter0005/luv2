9c1818e5a24fa0304f16dfa1f1dca42ff64b838c
pedrohenriqww <pedrohenriqww9@gmail.com>
2026-01-26 22:36:30 +0000

    quero a pagina inteieroa, of udno, roum degrade roxo e rosa, sem pesar m

--- src/app/InnerLayout.tsx
+++ src/app/InnerLayout.tsx
@@ -13,12 +13,13 @@ export default function InnerLayout({ children }: { children: React.ReactNode })
 
   return (
     <div className="relative w-full min-h-screen">
-      {/* STATIC BACKGROUND FOR THE ENTIRE PAGE */}
+      {/* BACKGROUND GRADIENTE PROFISSIONAL E PERFORMANTE */}
       <div className="fixed inset-0 -z-10 overflow-hidden">
-        <div className="absolute inset-0 bg-[#05000a]"></div>
-        <div className="absolute top-[-20%] left-1/2 -translate-x-1/2 w-[80%] h-[800px] bg-purple-900/20 blur-[120px] rounded-full"></div>
-        <div className="absolute bottom-[-10%] right-0 w-[600px] h-[600px] bg-pink-600/10 blur-[100px] rounded-full"></div>
-        <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 mix-blend-overlay"></div>
+        <div className="absolute inset-0 bg-[#0a0313]" />
+        <div className="absolute -top-1/4 -left-1/4 w-1/2 h-1/2 bg-purple-600/30 blur-[120px] rounded-full" />
+        <div className="absolute -bottom-1/4 -right-1/4 w-1/2 h-1/2 bg-pink-500/30 blur-[120px] rounded-full" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 h-3/4 bg-indigo-500/10 blur-[100px] rounded-full" />
+        <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-10 mix-blend-soft-light" />
       </div>
       
       <div className="relative z-10 flex flex-col min-h-screen">
