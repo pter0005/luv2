2d79c34e0b59a7d1cd6c5d8beecf6a67ab82780e
pedrohenriqww <pedrohenriqww9@gmail.com>
2026-02-02 22:45:50 +0000

    na logo da footer , quero essa imagem aqui https://res.cloudinary.com/dn

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'storage.googleapis.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
