8898fe3019016493c87beaad6474a2773a4442a2
pedrohenriqww <pedrohenriqww9@gmail.com>
2026-02-13 05:12:37 +0000

    explica esse erro ai, n é isso q vc tenteiu n parece algo relacionado as

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -16,6 +16,8 @@ import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 // Função para formatar dinheiro
 const formatCurrency = (value: number) => {
   return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
