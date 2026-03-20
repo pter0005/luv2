36e5452ec5c80590ea24be1ef4dac598b9cfa699
pedrohenriqww <pedrohenriqww9@gmail.com>
2026-03-20 18:39:08 +0000

    aaaaa

--- src/components/layout/DemoSection.tsx
+++ src/components/layout/DemoSection.tsx
@@ -67,7 +67,7 @@ const DemoSection = () => {
                         </motion.p>
 
                         <motion.div initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.4 }} className="mt-10">
-                            <a target="_blank" rel="noopener noreferrer" href="https://mycupid.com.br/p/WgZtB23Y4OgatZPdrShO">
+                            <a target="_blank" rel="noopener noreferrer" href="https://mycupid.com.br/p/CLOGMrAk66u79KpyJHRK">
                                 <button className="relative inline-flex h-14 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 focus:ring-offset-slate-50 shadow-[0_0_40px_-10px_rgba(168,85,247,0.5)] group hover:scale-105 transition-transform duration-300">
                                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"></span>
                                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black/90 px-8 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-3">
