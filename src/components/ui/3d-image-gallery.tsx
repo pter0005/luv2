8fa0744bc99dcd66512d11d3e88a1f86743263c4
pedrohenriqww <pedrohenriqww9@gmail.com>
2026-04-15 19:46:44 +0000

    dadwd

--- src/components/ui/3d-image-gallery.tsx
+++ src/components/ui/3d-image-gallery.tsx
@@ -116,7 +116,7 @@ const FloatingCard = React.memo(function FloatingCard({
 
       <Html
         transform
-        occlude="blending"
+        occlude={[occludeRef]}
         distanceFactor={8}
         position={[0, 0, 0.1]}
         zIndexRange={[100, 0]}
